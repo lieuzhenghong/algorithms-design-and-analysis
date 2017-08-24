3649938ea72f86db0d1b246ba150ac9833aff616
lieu <lieuzhenghong@gmail.com>
2017-08-24 19:31:13 +0800

    Fix code so it's actually correct (didn't count properly in sort_array)

--- I/assignment-1/inversions.py
+++ I/assignment-1/inversions.py
@@ -20,11 +20,17 @@ def read_file():
 
 def sort_array(a):
     inversions = 0
+    # The array is either of length 3 or 2 
+    for i in range(len(a)-1):
+        for j in range(i, len(a)):
+            if a[i] > a[j]:
+                inversions +=1
     for i in range(len(a)):
         mini = min(a[i:]) #find minimum element
         min_index = a[i:].index(mini) #find index of minimum element
         if (min_index is not 0):
-            inversions += 1
+            #inversions +=1
+            pass
         a[i + min_index] = a[i] #replace element at min_index with first element
         a[i] = mini                  #replace first element with min element
     return (a, inversions)
@@ -44,6 +50,7 @@ def count_inversions(array):
         result_b = count_inversions(b)
         s_a = result_a[0]
         s_b = result_b[0]
+        #print(result_a, result_b)
         inversions += result_a[1]
         inversions += result_b[1]
         pointer_a = 0
@@ -71,8 +78,24 @@ def count_inversions(array):
                 pointer_b += 1
                 inversions += len(s_a[pointer_a:]) # Remaining elements in s_a
     return (sorted_array, inversions)
-
+test_1 = [1,3,5,2,4,6]
+test_2 = [1,5,3,2,4]
+test_3 = [5,4,3,2,1]
+test_4 = [1,6,3,2,4,5]
+test_15 = [ 9, 12, 3, 1, 6, 8, 2, 5, 14, 13, 11, 7, 10, 4, 0 ]
+test_50 = [37, 7, 2, 14, 35, 47, 10, 24, 44, 17, 34, 11, 16, 48, 1, 39, 6, 33,
+43, 26, 40, 4, 28, 5, 38, 41, 42, 12, 13, 21, 29, 18, 3, 19, 0, 32, 46, 27, 31,
+25, 15, 36, 20, 8, 9, 49, 22, 23, 30, 45 ]
+test_100 = [ 4, 80, 70, 23, 9, 60, 68, 27, 66, 78, 12, 40, 52, 53, 44, 8, 49, 28, 18, 46, 21, 39, 51, 7, 87, 99, 69, 62, 84, 6, 79, 67, 14, 98, 83, 0, 96, 5, 82, 10, 26, 48, 3, 2, 15, 92, 11, 55, 63, 97, 43, 45, 81, 42, 95, 20, 25, 74, 24, 72, 91, 35, 86, 19, 75, 58, 71, 47, 76, 59, 64, 93, 17, 50, 56, 94, 90, 89, 32, 37, 34, 65, 1, 73, 41, 36, 57, 77, 30, 22, 13, 29, 38, 16, 88, 61, 31, 85, 33, 54 ]
 start = timeit.default_timer()
 print(count_inversions(read_file())[1], 'inversions')
+#print(count_inversions(test_1)[1], 'inversions; expecting 3')
+#print(count_inversions(test_2)[1], 'inversions; expecting 4')
+#print(count_inversions(test_3)[1], 'inversions; expecting 10')
+#print(count_inversions(test_4)[1], 'inversions; expecting 5')
+#print(count_inversions(test_15)[1], 'inversions; expecting 56')
+#print(count_inversions(test_50)[1], 'inversions; expecting 590')
+#print(count_inversions(test_100)[1], 'inversions; expecting 2372')
 stop = timeit.default_timer()
 print ('Time taken: {0} seconds'.format(stop-start))
+
