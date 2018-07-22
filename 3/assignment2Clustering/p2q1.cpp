ef69fac9cf9e7d85c98af251d17a3966a15063af
Lieu Zheng Hong <lieuzhenghong@gmail.com>
2018-07-22 13:22:34 +0800

    Add working solution for question 1

--- 3/assignment2Clustering/p2q1.cpp
+++ 3/assignment2Clustering/p2q1.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <queue>
 #include <array>
+#include <chrono>
 
 /** 
  * In this programming problem and the next you'll code up the clustering
@@ -32,21 +33,24 @@
 
 typedef std::array<int, 3> edge;
 
+const int MAX_VERTS = 2000;
+
 // This function, when run on a vertex v, guarantees that it and all its parents
 // will point to its root leader.
-void compress(std::array <int, 1000> leaders, int v) {
+void compress(std::array <int, MAX_VERTS>& leaders, int v) {
     std::vector<int> vs_to_update;
-    int curr_v = v;
+    int curr_v = v;	
 
     // Walk up the vertex tree until we find the leader (vert points to itself)
     while(leaders[curr_v] != curr_v) {
+		//std::cout << "Current vertex: " << curr_v << " pointing to " << leaders[curr_v] << std::endl;
         vs_to_update.push_back(curr_v);
         curr_v = leaders[curr_v];
     }
 
     // Now compress all vertices
     for (auto w : vs_to_update) {
-        std::cout << "Vertex: " << w << std::endl;
+        //std::cout << "Updating vertex " << w << " to point at " << curr_v << std::endl;
         leaders[w] = curr_v;
     }
 }
@@ -61,7 +65,13 @@ int main() {
     int dist;
 
     int max_dist;
-    std::array <int, 1000> leaders;
+    std::array <int, MAX_VERTS> leaders;
+
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+	for (int i = 0; i < MAX_VERTS; i++) {
+		leaders[i] = i;
+	}
 
     auto cmp = [](edge a, edge b) { return (a[2] > b[2]); };
 
@@ -72,13 +82,13 @@ int main() {
     > edges(cmp);
 
     std::cin >> num_vertices;
+	num_clusters = num_vertices;
 
     while(std::cin) {
         std::cin >> source >> end >> dist;
-        std::cout << source << " " << end << " " << dist << std::endl;
+        //std::cout << source << " " << end << " " << dist << std::endl;
         edges.push({source, end, dist});
         leaders[source] = source;
-        num_clusters++;
     }
 
     // Why >= ? Because we want to stop at the node that brings it from
@@ -86,7 +96,7 @@ int main() {
     // vertices that's not in a cluster.
 
     while (num_clusters >= target_clusters) {
-        std::cout << "Number of clusters: " << num_clusters << std::endl;
+        //std::cout << "Number of clusters: " << num_clusters << std::endl;
 
         source = edges.top()[0];
         end = edges.top()[1];
@@ -94,12 +104,31 @@ int main() {
 
         edges.pop();
 
+		//std::cout << source << " " << end << " " << dist << std::endl;
+
         // Compress END: now guaranteed it points to LEADER(END)
-        std::cout << "Compressing leader..." << std::endl;
+        //std::cout << "Compressing leader..." << std::endl;
         compress(leaders, end);
         // Compress SOURCE
+
+		/*
+		std::cout << "leaders after end compression: " << std::endl;
+		for (int i = 1; i < num_vertices+1; i++) {
+			std::cout << leaders[i] << " ";
+		}
+		std::cout << std::endl;
+
         std::cout << "Compressing source..." << std::endl;
+		*/
         compress(leaders, source);
+
+		/*
+		std::cout << "leaders after source compression: " << std::endl;
+		for (int i = 1; i < num_vertices+1; i++) {
+			std::cout << leaders[i] << " ";
+		}
+		std::cout << std::endl;
+		*/
 
         // Now we have a guarantee that source and end both point to root leader
         // By comparing the source's leader to the end's leader, we can tell if
@@ -107,12 +136,22 @@ int main() {
         // if they are not identical, point the source leader to end leader, clusters--
 
         if (leaders[source] != leaders[end]) {
-            leaders[source] = leaders[end];
+            leaders[leaders[source]] = leaders[leaders[end]];
             max_dist = dist;
             num_clusters--;
+			/*
+			for (int i = 1; i < num_vertices+1; i++) {
+			std::cout << leaders[i] << " ";
+			}
+			std::cout << std::endl;
+			*/
         }
     }
 
+	std::cout << "Max distance: " << max_dist << std::endl;
+	std::chrono::steady_clock::time_point t_end= std::chrono::steady_clock::now();
+	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(t_end - begin).count() <<std::endl;
+	//std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (t_end - begin).count() <<std::endl;
     return max_dist;
 }
 
