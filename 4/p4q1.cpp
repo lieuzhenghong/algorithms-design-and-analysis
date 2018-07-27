af9dd35ca711511617bf429c179c9d42ceb66ad5
Lieu Zheng Hong <lieuzhenghong@gmail.com>
2018-07-28 01:42:15 +0800

    Modify p4q1 solution to use integers instead of doubles for much faster performance and parsimonious code

--- 4/p4q1.cpp
+++ 4/p4q1.cpp
@@ -5,6 +5,7 @@
 #include <map>
 #include <algorithm>
 #include <chrono>
+#include <climits>
 
 /*
 In this assignment you will implement one or more algorithms for the
@@ -30,33 +31,52 @@ you have extra time, try comparing the performance of different all-pairs
 shortest-path algorithms!
 */
 
-
-// Implement Bellman-Ford (one pass), then implement Dijkstra's, which I've
-// already coded up before in Python
-
-// 1. Bellman-Ford
+// The smart way 
+// 1. Bellman-Ford with source vertex 0 
 // 2. Use Johnson's algorithm to reweight vertices
 // 3. Use Dijkstra's algorithm N times for each vertex
 
-typedef std::array<double, 3> edge;
+// The naive way (this implementation)
+// 1. Run Bellman-Ford n times
+
+struct edge {
+    int source;
+    int end;
+    int cost;
+};
+
+struct pair {
+    int source;
+    int end;
+};
+
+
 typedef std::vector<edge> vector_of_edges;
 
 int num_vertices;
 int num_edges;
-double costs[3000][3000] {std::numeric_limits<double>::infinity()};
+
+int costs[3000][3000] {INT_MAX};
+
+//double costs[3000][3000] {std::numeric_limits<double>::infinity()};
 std::map<int, vector_of_edges> all_edges;
 
 bool compLength(edge a, edge b) {
-    return (a[2] < b[2]);
+    return (a.cost < b.cost);
 }
 
-std::array<double, 3> bellmanFord(int vertex_source) {
+edge bellmanFord(int vertex_source) {
 
-    double best_cost = std::numeric_limits<double>::infinity();
-    std::array<double , 2> best_two_pair = {0, 0};
+    for (int i = 0; i < 3000; i++) {
+        for (int j = 0; j < 3000; j++) {
+            costs[i][j] = INT_MAX;
+        }
+    }
+
+    edge best_edge = {0, 0, INT_MAX};
 
     for (int i = 0; i < num_vertices; i++) {
-        for (int v = 1; v < num_vertices+1; v++) {
+        for (int v = 0; v < num_vertices+1; v++) {
             /*
             std::cout << "Currently considering the path from node " <<
             vertex_source << " to " << v << " using " << i << " edges max" <<std::endl;
@@ -66,58 +86,69 @@ std::array<double, 3> bellmanFord(int vertex_source) {
                     costs[i][v]  = 0;
                 }
                 else {
-                    costs[i][v] = std::numeric_limits<double>::infinity();
+                    costs[i][v] = INT_MAX;
                 }
-                // std::cout << "Cost: " << costs[i][v] << std::endl;
             }
             else {
                 // Two cases: either equivalent to costs[i-1][v], or equal to costs[i-1][w] + cost(w-v) for all W that point to V.
-                double min_cost = costs[i-1][v];
-                // std::cout << "Min cost: " << costs[i-1][v] << std::endl;
-                for (int e = 0; e < all_edges[v].size(); e++) {
-                    double current_cost = all_edges[v][e][2];
-                    int w = all_edges[v][e][0];
-                    current_cost += costs[i-1][w];
-                    if (current_cost < min_cost) {min_cost = current_cost;}
+                int prev_cost = costs[i-1][v];
+                /*
+                std::cout << "Current cost for pair" << vertex_source << v <<
+                costs[i][v] << std::endl;
+                std::cout << "Min cost: " << costs[i-1][v] << std::endl;
+                */
+
+               auto min_cost = prev_cost;
+
+               for (auto edge : all_edges[v]) {
+                    //std::cout << "Edge: " << edge.source << " " << edge.end << std::endl;
+                    auto source_cost = costs[i-1][edge.source];
+                    int total_cost;
+
+                    if (source_cost == INT_MAX) {
+
+                    }
+                    else {
+                        auto total_cost = source_cost + edge.cost; //w + c(w-v)
+                        if (total_cost < min_cost) {
+                            min_cost = total_cost;
+                        }
+                        if (min_cost < best_edge.cost) {
+                            /*
+                            std::cout << "Best edge found!" << vertex_source << " " << v 
+                            << " " <<min_cost
+                            << std::endl;
+                            */
+                            best_edge = {vertex_source, v, min_cost};
+                        }
+                    }
                 }
                 costs[i][v] = min_cost;
-                if (min_cost < best_cost) { 
-                    best_cost = min_cost;
-                    best_two_pair = {vertex_source, v};
-                };
-                //std::cout << "New Min cost: " << min_cost << std::endl;
+                //std::cout << "Best cost: " << costs[i][v] << std::endl;
             }
         }
     }
 
-    /*
-    std::cout << "Best cost: " << best_cost << std::endl;
-    std::cout << "Best two pair: " << best_two_pair[0] << " , " << best_two_pair[1] << std::endl;
-    */
-
-    // Now we run with ONE more edge : i.e. i = num_vertices
-    for (int v = 1; v < num_vertices+1; v++) {
+    // run again to check for negative cycles
+    for (int v = 0; v < num_vertices; v++) {
         int i = num_vertices;
-        /*
-        std::cout << "Currently considering the path from node " <<
-        vertex_source << " to " << v << " using " << i << " edges max" <<std::endl;
-        */
-        double min_cost = costs[i-1][v];
-        //std::cout << "Min cost: " << costs[i-1][v] << std::endl;
-        for (int e = 0; e < all_edges[v].size(); e++) {
-            double current_cost = all_edges[v][e][2];
-            int w = all_edges[v][e][0];
-            current_cost += costs[i-1][w];
-            if (current_cost < min_cost) {min_cost = current_cost;}
-        }
-        costs[i][v] = min_cost;
-        // std::cout << "New Min cost: " << min_cost << std::endl;
-        if (min_cost < best_cost) { 
-            // std::cout << "Negative cycle detected! " << std::endl;
-            return {0, 0, 0};
+        // For all edges that point to the vertex
+        for (auto edge : all_edges[v]) {
+            if (costs[i-1][edge.source] != INT_MAX && 
+            (costs[i-1][edge.source] + edge.cost) < best_edge.cost) {
+                std::cout << "There's a negative cycle!" << std::endl;
+                return {-1, -1, INT_MAX};
+            }
         }
     }
-    return { best_two_pair[0] , (int)best_two_pair[1], best_cost};
+
+
+    /*
+    std::cout << "Best edge: " << best_edge.source << " " << best_edge.end <<
+    " " << best_edge.cost << std::endl;
+    */
+
+    return best_edge;
 }
 
 //GOTCHAS 
@@ -126,20 +157,16 @@ std::array<double, 3> bellmanFord(int vertex_source) {
 
 int main() {
     std::ios_base::sync_with_stdio(false); 
-	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+	auto time_started = std::chrono::steady_clock::now();
     // First run Bellman-Ford. We keep a 2D array of i (number of edges we're allowed to use) and v (all vertices).
     std::cin >> num_vertices >> num_edges;
 
     // Fill in the edges in the following format:
-    int source;
-    int end;
-    int cost;
+    edge e;
 
-    while (std::cin) {
-        std::cin >> source >> end >> cost;
-        edge e = {source, end, cost};
+    while (std::cin >> e.source >> e.end >> e.cost) {
         // For each vertex, We push to array all edges that point to it
-        all_edges[end].push_back(e);
+        all_edges[e.end].push_back(e);
         //std::cout << e[0] << " " << e[1] << " " << e[2] << std::endl;
     }
 
@@ -148,37 +175,34 @@ int main() {
         std::sort(all_edges[v].begin(), all_edges[v].end(), compLength);
     }
 
-    // Just for debugging purpose
-    
-    for (int v = 1; v < num_vertices+1; v++) {
-        /*
-        for (int e = 0; e < all_edges[v].size(); e++) {
-            std::cout << 
-            all_edges[v][e][0] << " " << 
-            all_edges[v][e][1] << " " <<
-            all_edges[v][e][2] << 
-            std::endl;
-        }
-        */
-    }
+    // We run Bellman-Ford's algorithm n times to final the shortest pair ever
+    //std::array<double, 3> best_values = {std::numeric_limits<double>::infinity()};
+    edge best_edge = {INT_MAX};
 
-    // Running Bellman-Ford, let's use vertex 1 as the source.
-    std::array<double, 3> best_values = {std::numeric_limits<double>::infinity()};
     for (int v = 1; v < num_vertices+1; v++) {
         std::cout << "Checking vertex " << v << std::endl;
-        std::array<double,3> current_values = bellmanFord(v);
-        if (current_values[0] == 0 ) {
+        edge current_edge = bellmanFord(v);
+        /*
+        std::cout << current_edge.source << " " <<current_edge.end << " " 
+        << current_edge.cost << std::endl;
+        */
+        if (current_edge.source == -1 ) {
             std::cout << "NULL" << std::endl;
             return 1;
         }
-        else if (current_values[2] < best_values[2]) {
-            best_values = current_values;
+        else if (current_edge.cost < best_edge.cost) {
+            best_edge = current_edge;
         }
     }
 
-    std::cout << "BEST two pair: " << best_values[0] << " , " <<
-    best_values[1] << std::endl; std::cout << "BEST cost: " << best_values[2]
-    << std::endl; std::chrono::steady_clock::time_point t_end=
-    std::chrono::steady_clock::now(); std::cout << "Time difference = " <<
-    std::chrono::duration_cast<std::chrono::milliseconds>(t_end -
-    begin).count() <<std::endl; return 0; }
+    std::cout << "BEST two pair: " << best_edge.source << " , " << best_edge.end << std::endl;
+    std::cout << "BEST cost: " << best_edge.cost << std::endl;
+
+    auto time_ended = std::chrono::steady_clock::now();
+
+    std::cout << "Time difference = " <<
+    std::chrono::duration_cast<std::chrono::milliseconds>(time_ended -
+    time_started).count() <<std::endl;
+
+    return 0;
+}
